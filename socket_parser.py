52cf76ba4274846a379eee30535cb3a0546e0bd1
Sergei Efimov <efimovsergeiv@gmail.com>
2023-03-24 16:28:37 +0300

    add:

--- socket_parser.py
+++ socket_parser.py
@@ -49,11 +49,10 @@ class TCPHandler(socketserver.BaseRequestHandler):
                         self.VALUES[f'{reg}{sec}'] = str_val
 
                 data_send = json.dumps(self.VALUES, ensure_ascii=False)
-                try:
-                    self.request.sendall(bytes(data_send, encoding="utf-8"))
-                    sleep(TIMEOUT)
-                except:
-                    pass
+
+                self.request.sendall(bytes(data_send, encoding="utf-8"))
+                sleep(TIMEOUT)
+
 
         except ConnectionAbortedError:
             print("")
